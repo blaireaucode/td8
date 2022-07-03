d1d6aa7515fda56e04f93d1a51b0b3c5c37dd0ae
Blaireaucode <blaireaucode@gmail.com>
2022-07-03 20:38:30 +0200

    test1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import {Provider} from 'react-redux';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
-import AllRoutes from './AllRoutes';
 import Theme from './theme.json';
 import store from "./helpers/store";
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import ScreenDebug from "./ScreenDebug";
-import Layout from "./Layout";
-import LayoutBothBar from "./LayoutBothBar";
-import {slide as Menu} from 'react-burger-menu'
 import Nav from "./Nav2";
 
 
@@ -18,16 +13,20 @@ const theme = createTheme(Theme);
 const classes = {}
 
 
+/*
+               <Routes>
+                        <Route path="/" element={<ScreenDebug/>}/>
+                    </Routes>
+
+ */
 function App() {
 
     return (
         <Provider store={store}>
             <BrowserRouter>
-                <Nav/>
+                {/*<Nav/>*/}
                 <div className="App">
-                    <Routes>
-                        <Route path="/" element={<ScreenDebug/>}/>
-                    </Routes>
+                    ok
                 </div>
 
             </BrowserRouter>
