354bfb09dcd5fa59ab8be3a80abbb307cc4ac9d9
Blaireaucode <blaireaucode@gmail.com>
2022-07-17 22:23:09 +0200

    dices v1 ok

--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -16,11 +16,18 @@ const diceBox = new DiceBox("#dice-box",
     {
         id: "dice-canvas", // canvas element id
         assetPath: "/assets/dice-box/",
-        startingHeight: 6,
-        throwForce: 4,
+        startingHeight: 7,
+        throwForce: 6,
         spinForce: 5,
         lightIntensity: 0.9,
-        scale: 5
+        scale: 6,
+        gravity: 1,
+        mass: 4,
+        friction: 0.8,
+        restitution: 0.1, // 0.6
+        linearDamping: 0.3, // 0.5
+        angularDamping: 0.2, // 0.2
+        settleTimeout: 3000
     }
 );
 
@@ -34,38 +41,45 @@ class RollDice extends Component {
     constructor(props) {
         super(props);
         this.throw_dice = this.throw_dice.bind(this);
-        this.get_result = this.get_result.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-        this.state = {dice_flag: false};
+        this.get_dice_result = this.get_dice_result.bind(this);
+        this.remove_dice = this.remove_dice.bind(this);
+        this.state = {dice_result: false};
     }
 
     throw_dice() {
         console.log('dice go');
-        this.setState({dice_flag: true});
-        diceBox.roll('1d20');
+        this.setState({dice_result: false});
+        diceBox.roll('5d6');
         diceBox.onRollComplete = (results) => {
-            console.log('results', results);
-            this.get_result(results);
+
+            console.log('onRollComplete');
+            this.get_dice_result(results);
         }
         diceBox.show();
     }
 
-    get_result(results) {
+    get_dice_result(results) {
         console.log('get results', results);
+        this.setState({dice_result: results});
     }
 
-    handleClose() {
+    remove_dice() {
         console.log('close');
-        this.setState({dice_flag: false})
+        this.setState({dice_result: false})
         diceBox.hide();
     }
 
     render() {
         console.log('state', this.state, diceBox);
+        let r = '';
+        if (this.state.dice_result) {
+            r = this.state.dice_result[0].rolls[0].value; // total
+            console.log('dice value', r);
+        }
         return (
             <div>
                 <L onClick={this.throw_dice}> dice </L> /
-                <L onClick={this.handleClose}> close </L>
+                <L onClick={this.remove_dice}> close </L> {r}
             </div>
         );
     }
